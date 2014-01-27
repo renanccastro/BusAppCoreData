af146e8c5d95762c67bf10401aff3ef69ac6eb7f
Renan <renanccastro@gmail.com>
2014-01-27 14:02:27 -0200

    Initial Commit, adding new files and core data integration.

---
+++ BusAppCoreData/BusAppCoreData/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  BusAppCoreData
+//
+//  Created by Renan Camargo de Castro on 27/01/14.
+//  Copyright (c) 2014 BEPiD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
