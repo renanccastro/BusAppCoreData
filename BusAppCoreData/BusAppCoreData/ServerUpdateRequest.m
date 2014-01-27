10f4e38e8f813201180fb2c6eb0b37d095e39ab7
Flavio Matheus Muniz Ribeiro da Silva <flaviommrs@gmail.com>
2014-01-27 15:22:13 -0200

    Request Classes and Supervisor methods

---
+++ BusAppCoreData/BusAppCoreData/ServerUpdateRequest.m
@@ -0,0 +1,13 @@
+//
+//  ServerUpdateRequest.m
+//  BusAppCoreData
+//
+//  Created by Flavio Matheus on 27/01/14.
+//  Copyright (c) 2014 BEPiD. All rights reserved.
+//
+
+#import "ServerUpdateRequest.h"
+
+@implementation ServerUpdateRequest
+
+@end
